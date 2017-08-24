06ac2b0a3352f42dcd662748f783a6ace79e285e
Aaron Britton <aaron.britton@hotmail.com>
2017-08-24 18:50:52 +0100

    Added existing files

---
+++ Source/Fasetto.Word/DataModels/ApplicationPage.cs
@@ -0,0 +1,18 @@
+﻿namespace Fasetto.Word
+{
+    /// <summary>
+    /// A page of the application
+    /// </summary>
+    public enum ApplicationPage
+    {
+        /// <summary>
+        /// The initial login page
+        /// </summary>
+        Login = 0,
+
+        /// <summary>
+        /// The main chat page
+        /// </summary>
+        Chat = 1,
+    }
+}
